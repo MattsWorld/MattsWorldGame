95060bc4dd953ca6ae7e10920eee89489f6dcdbe
anthony.joseph.greer <anthony.joseph.greer@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-03 22:41:57 +0000

    Fancy Menu!

--- tests/SuperMattsWorld/src/org/myname/flixeldemo/MenuState.java
+++ tests/SuperMattsWorld/src/org/myname/flixeldemo/MenuState.java
@@ -1,6 +1,8 @@
 package org.myname.flixeldemo;
 
-import org.flixel.*;
+import org.flixel.FlxG;
+import org.flixel.FlxState;
+import org.flixel.FlxText;
 
 import android.view.KeyEvent;
 
@@ -8,8 +10,14 @@ public class MenuState extends FlxState
 {
 	public MenuState()
 	{
-		super();
-		add(new FlxText(10, 10, 250, "Press the center DPad key to continue."));
+		int titlex = 60;
+		int titley = 100;
+		FlxText text = new FlxText(titlex,  titley, 250, "Super Matt's World!");
+		text.setSize(22);
+		super.add(text);
+		text = new FlxText(titlex + 10, titley + 100, 250, "Press the center DPad key to continue.");
+		text.flicker(1F);
+		super.add(text);
 	}
 	
 	public void update()
