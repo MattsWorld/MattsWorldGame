9c07fc820e3ecf474470c9e9153112c4bf03491e
anthony.joseph.greer <anthony.joseph.greer@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-03 22:41:30 +0000

    Changed gravity

--- trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/Player.java
+++ trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/Player.java
@@ -2,9 +2,11 @@ package org.myname.flixeldemo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import android.view.*;
 
-import org.flixel.*;
+import org.flixel.FlxG;
+import org.flixel.FlxSprite;
+
+import android.view.KeyEvent;
 
 public class Player extends FlxSprite
 {
@@ -15,18 +17,20 @@ public class Player extends FlxSprite
 	protected static final int PLAYER_HEIGHT_PX = 16;
 	
 	protected static final int PLAYER_RUN_SPEED = 80;
-	protected static final float GRAVITY_ACCELERATION = 420;
-	protected static final float JUMP_ACCELERATION = 200;
+	protected static final float GRAVITY_ACCELERATION = 300; //-- 420
+	protected static final float JUMP_ACCELERATION = 300; //-- 400
 	
 	public Player()
 	{
 		super(PLAYER_START_X, PLAYER_START_Y, R.drawable.spaceman, true, PLAYER_WIDTH_PX, PLAYER_HEIGHT_PX);
+		//super.offset = new Point(16, 0);
+		
 		
 		drag.x = PLAYER_RUN_SPEED * 8;
 		acceleration.y = GRAVITY_ACCELERATION;
 		maxVelocity.x = PLAYER_RUN_SPEED;
 		maxVelocity.y = JUMP_ACCELERATION;
-		
+				
 		addAnimation("idle", new ArrayList<Integer>(Arrays.asList(new Integer[] {0})));
 		addAnimation("run",  new ArrayList<Integer>(Arrays.asList(new Integer[] {1, 2, 3, 0})), 12);
 		addAnimation("jump",  new ArrayList<Integer>(Arrays.asList(new Integer[] {5})));
@@ -39,17 +43,17 @@ public class Player extends FlxSprite
 	public void update()
 	{
 		acceleration.x = 0;
-		if(FlxG.keys.pressed(KeyEvent.KEYCODE_DPAD_LEFT))
+		if(FlxG.keys.pressed(KeyEvent.KEYCODE_DPAD_LEFT) || FlxG.keys.justPressed(KeyEvent.KEYCODE_A))
 		{
 			setFacing(LEFT);
 			acceleration.x = -drag.x;
 		}
-		else if(FlxG.keys.pressed(KeyEvent.KEYCODE_DPAD_RIGHT))
+		else if(FlxG.keys.pressed(KeyEvent.KEYCODE_DPAD_RIGHT) || FlxG.keys.justPressed(KeyEvent.KEYCODE_D))
 		{
 			setFacing(RIGHT);
 			acceleration.x = drag.x;
 		}
-		if(FlxG.keys.justPressed(KeyEvent.KEYCODE_DPAD_UP) && velocity.y==0)
+		if((FlxG.keys.justPressed(KeyEvent.KEYCODE_DPAD_UP) || FlxG.keys.justPressed(KeyEvent.KEYCODE_L)) && velocity.y==0)
 		{
 			velocity.y = -JUMP_ACCELERATION;
 		}
