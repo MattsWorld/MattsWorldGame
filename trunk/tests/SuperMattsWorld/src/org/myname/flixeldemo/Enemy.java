75522c625bd49c2d31888b5fa4464b82dec34598
anthony.joseph.greer <anthony.joseph.greer@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-04 18:00:52 +0000

    Made sounds static members to player classes

--- trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/Enemy.java
+++ trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/Enemy.java
@@ -7,28 +7,31 @@ import org.flixel.*;
 
 public class Enemy extends FlxSprite
 {	
-	protected static final float GRAVITY_ACCELERATION = 420;
+	private static final float GRAVITY_ACCELERATION = 420;
+	private static final FlxSound yupyup = new FlxSound().loadEmbedded(R.raw.yup_yup);
+
 	protected float speed;
-	
+
 	public Enemy(int startX, int startY, float horizontalSpeed, int SimpleGraphic)
 	{
 		super(startX, startY, SimpleGraphic, true);
-		
-		speed = horizontalSpeed;
+		this.speed = horizontalSpeed;
 		this.velocity.x = speed;
 		this.acceleration.y = GRAVITY_ACCELERATION;
-		addAnimation("idle", new ArrayList<Integer>(Arrays.asList(new Integer[] {0, 1})), 12);
+		super.addAnimation("idle", new ArrayList<Integer>(Arrays.asList(new Integer[] {0, 1})), 12);
 	}
 	
 	public void update()
 	{
+		//-- Don't move the character whilest
+		//   off the screen.
 		if(!this.onScreen())
 		{
 			this.velocity.x = 0;
 		}
 		else
 		{
-			play("idle");
+			super.play("idle");
 			this.velocity.x = speed;
 		}
 
@@ -41,11 +44,11 @@ public class Enemy extends FlxSprite
 
 		if(collidedX)
 		{
-			FlxG.play(R.raw.yup_yup);
+			yupyup.play();
 			this.speed = -this.speed;
 			this.velocity.x = this.speed;
 		}
-		
+
 		return collidedX;
 	}
 }
