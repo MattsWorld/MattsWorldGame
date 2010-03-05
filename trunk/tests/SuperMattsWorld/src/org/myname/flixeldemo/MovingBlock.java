c982b1a3d6239e66335e47d008a7b171785a30f7
pjf44055 <pjf44055@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-05 08:48:55 +0000

    Can implement bi-directional platforms (e.g. left to right or up and down), as well as single-direction platforms (e.g. down only or right only). Diagonal platforms are also possible for both types.

--- trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/MovingBlock.java
+++ trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/MovingBlock.java
@@ -12,9 +12,10 @@ public class MovingBlock extends FlxBlock
 	protected int horizontalMovementSpeed;
 	protected int startX;
 	protected int startY;
+	protected boolean oneway;
 	
 	public MovingBlock(int maxVerticalMovement, int verticalSpeed, int maxHorizontalMovement, 
-			int horizontalSpeed, int xPos, int yPos, int width, int height)
+			int horizontalSpeed, int xPos, int yPos, int width, int height, boolean oneway)
 	{
 		super(xPos, yPos, width, height);
 		this.verticalMovementDirection = 1;
@@ -25,6 +26,7 @@ public class MovingBlock extends FlxBlock
 		this.horizontalMovementSpeed = horizontalSpeed;
 		this.startX = xPos;
 		this.startY = yPos;	
+		this.oneway = oneway;
 	}
 	
 	public void update()
@@ -33,25 +35,110 @@ public class MovingBlock extends FlxBlock
 		
 		this.y += verticalMovementDirection * verticalMovementSpeed;
 		this.x += horizontalMovementDirection * horizontalMovementSpeed;
-			   
-		if (this.x >= this.startX + this.maximumHorizontalMovement)
-		{
-		    this.x = this.startX + this.maximumHorizontalMovement;
-		    horizontalMovementDirection = -1;
-		}
-		else if (this.x <= this.startX)
-		{
-		    horizontalMovementDirection = 1;
-		}
 		
-		if (this.y >= this.startY + this.maximumVerticalMovement)
+		if(!oneway) //both directions (diagonals work as well)
 		{
-		    this.y = this.startY + this.maximumVerticalMovement;
-		    verticalMovementDirection = -1;
+			if(maximumHorizontalMovement > 0) //left (start) to right (end); works
+			{
+				if (this.x >= this.startX + this.maximumHorizontalMovement) //right of end position
+				{
+				    this.x = this.startX + this.maximumHorizontalMovement;
+				    horizontalMovementDirection = -1;
+				}
+				else if (this.x <= this.startX) // left of start position
+				{
+				    horizontalMovementDirection = 1;
+				}
+			}
+			else if (maximumHorizontalMovement < 0) //right (start) to left (end); works
+			{
+				if(this.x >= this.startX) //right of start position
+				{
+					horizontalMovementDirection = -1;
+				}
+				else if(this.x <= this.startX + this.maximumHorizontalMovement) //left of end position
+				{
+					this.x = this.startX + this.maximumHorizontalMovement;
+					horizontalMovementDirection = 1;
+				}
+			}
+			
+			if(maximumVerticalMovement > 0) //up (start) to down (end); works
+			{
+				if (this.y >= this.startY + this.maximumVerticalMovement) //above end location
+				{
+				    this.y = this.startY + this.maximumVerticalMovement;
+				    verticalMovementDirection = -1;
+				}
+				else if (this.y <= this.startY) //below start position
+				{
+				    verticalMovementDirection = 1;
+				}
+			}
+			else if(maximumVerticalMovement < 0) //down (start) to up (end); works
+			{
+				if(this.y >= this.startY) //above start position
+				{
+					verticalMovementDirection = -1;
+				}
+				else if(this.y <= this.startY + this.maximumVerticalMovement) //below end position
+				{
+					this.y = this.startY + this.maximumVerticalMovement;
+					verticalMovementDirection = 1;
+				}
+			}
 		}
-		else if (this.y <= this.startY)
+		else //single directions only (diagonals work as well)
 		{
-		    verticalMovementDirection = 1;
+			if(maximumHorizontalMovement < 0) //left only; works
+			{
+				if(this.x >= this.startX) //right of start position
+				{
+					horizontalMovementDirection = -1;
+				}
+				else if(this.x <= this.startX + this.maximumHorizontalMovement) //left of end position
+				{
+					this.x = this.startX - this.maximumHorizontalMovement;
+					horizontalMovementDirection = -1;
+				}
+			}
+			else if(maximumHorizontalMovement > 0) //right only; works
+			{
+				if (this.x >= this.startX + this.maximumHorizontalMovement) //right of end position
+				{
+				    this.x = this.startX - this.maximumHorizontalMovement;
+				    horizontalMovementDirection = 1;
+				}
+				else if (this.x <= this.startX) // left of start position
+				{
+				    horizontalMovementDirection = 1;
+				}
+			}
+			
+			if(maximumVerticalMovement < 0) //up only; works
+			{
+				if(this.y >= this.startY) //above start position
+				{
+					verticalMovementDirection = -1;
+				}
+				else if(this.y <= this.startY + this.maximumVerticalMovement) //below end position
+				{
+					this.y = this.startY - this.maximumVerticalMovement;
+					verticalMovementDirection = -1;
+				}
+			}
+			else if(maximumVerticalMovement > 0) //down only; works
+			{
+				if (this.y >= this.startY + this.maximumVerticalMovement) //above end location
+				{
+				    this.y = this.startY - this.maximumVerticalMovement;
+				    verticalMovementDirection = 1;
+				}
+				else if (this.y <= this.startY) //below start position
+				{
+				    verticalMovementDirection = 1;
+				}
+			}
 		}
 	}
 }
