7df7482c7307df8886051b50ef60a617b8befb3d
ghughes06@yahoo.com <ghughes06@yahoo.com@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-07 00:05:39 +0000

    Added DeathBlocks to the game. DeathBlocks can animated. FlxBlock has no built in way to animate, so I had to create AnimatedBlock using code from FlxSprite to get blocks to be animated. Added spikes, water and fire to the end of the level as an example.

--- trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/GameState.java
+++ trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/GameState.java
@@ -7,22 +7,29 @@ import org.flixel.FlxCollideListener;
 import org.flixel.FlxCore;
 import org.flixel.FlxG;
 import org.flixel.FlxState;
+import org.myname.flixeldemo.DeathBlock.BlockType;
 
 public class GameState extends FlxState 
 {
 	protected ArrayList<FlxBlock> levelBlocks = new ArrayList<FlxBlock>();
 	protected ArrayList<FlxBlock> highBlocks = new ArrayList<FlxBlock>();
 	protected ArrayList<MovingBlock> movingBlocks = new ArrayList<MovingBlock>();
+	protected ArrayList<DeathBlock> deathBlocks = new ArrayList<DeathBlock>(); 
 	protected ArrayList<Enemy> enemies	= new ArrayList<Enemy>();
 	protected Player player = null;
 
 	public GameState()
 	{		
-		FlxBlock ground = new FlxBlock(0, 640-16, 640, 16);
+		FlxBlock ground = new FlxBlock(0, 640-16, 825, 16);
 		FlxBlock higherGround = new FlxBlock(60, 640 - 60, 50, 16);
 		FlxBlock evenHigherGround = new FlxBlock( 75, 640 - 100, 50, 16);
 		FlxBlock leftWall = new FlxBlock(340, 640-32, 16, 16);
 		FlxBlock rightWall = new FlxBlock(540, 640-32, 16, 16);
+		FlxBlock righterWall = new FlxBlock(684, 640-32, 16, 16);
+		FlxBlock platformAboveDeath = new FlxBlock(625, 640 - 125, 159, 16);
+		DeathBlock spike = new DeathBlock(650, 640-141, 80, BlockType.spike);
+		DeathBlock water = new DeathBlock(556, 640-32, 128, BlockType.water);
+		DeathBlock flames = new DeathBlock(700, 640-32, 112, BlockType.fire);
 		MovingBlock platform = new MovingBlock(50, 1, 0, 0, 150, 640-64, 90, 48, false);
 		MovingBlock squisher = new MovingBlock(-98, 2, 0, 0, 275, 640-98, 75, 32, false);
 		MovingBlock tester = new MovingBlock(98, 2, 0, 0, 275, 640-98, 75, 32, false); //test changes
@@ -32,10 +39,15 @@ public class GameState extends FlxState
 		evenHigherGround.loadGraphic(R.drawable.tech_tiles);
 		leftWall.loadGraphic(R.drawable.tech_tiles);
 		rightWall.loadGraphic(R.drawable.tech_tiles);
+		righterWall.loadGraphic(R.drawable.tech_tiles);
+		platformAboveDeath.loadGraphic(R.drawable.tech_tiles);
 		platform.loadGraphic(R.drawable.tech_tiles);
 		squisher.loadGraphic(R.drawable.tech_tiles);
 		tester.loadGraphic(R.drawable.tech_tiles);
 
+		deathBlocks.add(this.add(spike));
+		deathBlocks.add(this.add(water));
+		deathBlocks.add(this.add(flames));
 		movingBlocks.add(this.add(tester));
 		movingBlocks.add(this.add(platform));
 		movingBlocks.add(this.add(squisher));
@@ -43,13 +55,15 @@ public class GameState extends FlxState
 		highBlocks.add(this.add(evenHigherGround));
 		highBlocks.add(this.add(leftWall));
 		highBlocks.add(this.add(rightWall));
+		highBlocks.add(this.add(righterWall));
+		highBlocks.add(this.add(platformAboveDeath));
 		levelBlocks.add(this.add(ground));
 
 		player = new Player();
 		this.add(player);
 		FlxG.follow(player, 2.5f);
 		FlxG.followAdjust(0.5f, 0.0f);
-		FlxG.followBounds(0, 0, 640, 640);
+		FlxG.followBounds(0, 0, 800, 640);
 
 		float enemySpeed = 50;
 		enemies.add(this.add(new Enemy(400, 640 - 50, enemySpeed, R.drawable.enemy)));
@@ -60,6 +74,7 @@ public class GameState extends FlxState
 	public void update()
 	{
 		super.update();
+		
 		FlxG.collideArrayList(levelBlocks, player);
 		FlxG.collideArrayList(highBlocks, player);
 		FlxG.collideArrayList(movingBlocks, player);
@@ -89,6 +104,13 @@ public class GameState extends FlxState
 			}
 		}		
 		);
-
+		FlxG.overlapArrayList(deathBlocks, player, new FlxCollideListener()
+		{
+			public void Collide(FlxCore object1, FlxCore object2)
+			{
+				player.kill();
+			}
+		}
+		);
 	}
 }
