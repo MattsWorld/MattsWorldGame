4baee668558e99ec501141a8a3625b7025d1bd25
roger.john.walker <roger.john.walker@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-11 15:51:43 +0000

    Adds Level Saves

--- trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/parsing/Level.java
+++ trunk/tests/SuperMattsWorld/src/org/myname/flixeldemo/parsing/Level.java
@@ -21,43 +21,105 @@ public class Level extends FlxState
 	protected int background;
 	protected String name;
 	protected int width, height;
+	
+	
+	public static int nextLevel = R.raw.lvl_test;
+	public static int currentLevel = R.raw.lvl_test;
+	public static String levelEntryLabel = "start";
+
+	
+	
+	public static HashMap<Integer, Level> levelSaves= new HashMap<Integer, Level>();
+	
+	
 
 	protected Player player;
-	protected final ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-	protected final HashMap<String, Point> labels = new HashMap<String, Point>();
+//	protected final ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+//	protected final HashMap<String, Point> labels = new HashMap<String, Point>();
+//	//-- TODO needs a container Object for inner variables.
+//	protected final ArrayList<Object> jump = new ArrayList<Object>();
+//	protected final ArrayList<FlxBlock> movingBlocks = new ArrayList<FlxBlock>();
+//	protected final ArrayList<FlxBlock> stationaryBlocks = new ArrayList<FlxBlock>();
+//	protected final ArrayList<FlxBlock> hurtBlocks = new ArrayList<FlxBlock>();
+//	protected final ArrayList<FlxBlock> deathBlocks = new ArrayList<FlxBlock>();
+
+	protected ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	protected HashMap<String, Point> labels = new HashMap<String, Point>();
 	//-- TODO needs a container Object for inner variables.
-	protected final ArrayList<Object> jump = new ArrayList<Object>();
-	protected final ArrayList<FlxBlock> movingBlocks = new ArrayList<FlxBlock>();
-	protected final ArrayList<FlxBlock> stationaryBlocks = new ArrayList<FlxBlock>();
-	protected final ArrayList<FlxBlock> hurtBlocks = new ArrayList<FlxBlock>();
-	protected final ArrayList<FlxBlock> deathBlocks = new ArrayList<FlxBlock>();
+	protected ArrayList<Object> jump = new ArrayList<Object>();
+	protected ArrayList<FlxBlock> movingBlocks = new ArrayList<FlxBlock>();
+	protected ArrayList<FlxBlock> stationaryBlocks = new ArrayList<FlxBlock>();
+	protected ArrayList<FlxBlock> hurtBlocks = new ArrayList<FlxBlock>();
+	protected ArrayList<FlxBlock> deathBlocks = new ArrayList<FlxBlock>();
 
 	public Level()
 	{
-		new LevelParser(this);
-
-		for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
-			super.add(it.next());
-
-		for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
-			super.add(it.next());
-
-		for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
-			super.add(it.next());
-
-		for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
-			super.add(it.next());
-
-		super.add(player);
-
-		for(Iterator<Enemy> it = enemies.iterator(); it.hasNext();)
-			super.add(it.next());
-
-		FlxG.follow(player, 2.5f);
-		FlxG.followAdjust(0.5f, 0.0f);
-		FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
+		if (!Level.levelSaves.containsKey(nextLevel)){
+			new LevelParser(this);
+	
+			for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			super.add(player);
+	
+			for(Iterator<Enemy> it = enemies.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			FlxG.follow(player, 2.5f);
+			FlxG.followAdjust(0.5f, 0.0f);
+			FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
+		}else{
+			
+			new LevelParser(this);
+			for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
+				super.add(it.next());
+			
+	
+			super.add(player);
+	
+			for(Iterator<Enemy> it = levelSaves.get(nextLevel).enemies.iterator(); it.hasNext();)
+				super.add(it.next());
+	
+			FlxG.follow(player, 2.5f);
+			FlxG.followAdjust(0.5f, 0.0f);
+			FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
+			
+			player.x = 400;
+			player.y = 100;
+			
+		
+		}
+		currentLevel = LevelParser.KEY_RESOURCE_ADDR.get(this.name);
+		levelSaves.put(currentLevel, this);
 	}
-
+	
+	
+	public static void switchLevel(String nextName, String nextLabel)
+	{
+		nextLevel = LevelParser.KEY_RESOURCE_ADDR.get(nextName);
+		if (FlxG.state instanceof Level)
+			levelSaves.put(LevelParser.KEY_RESOURCE_ADDR.get(((Level)FlxG.state).name), ((Level)FlxG.state));
+		FlxG.switchState(Level.class);
+		
+	}
 	@Override
 	public void update()
 	{
@@ -101,7 +163,8 @@ public class Level extends FlxState
 		{
 			public void Collide(FlxCore object1, FlxCore object2)
 			{
-				player.kill();
+				//player.kill();
+				Level.switchLevel("lvl_test3","");
 			}
 		}
 		);
@@ -117,9 +180,17 @@ public class Level extends FlxState
 			{
 				//-- TODO Enemy specific code here... May want to kill the enemy or hurt the player
 				//   based on a series of instanceof statements. 
-				player.kill();
+				//player.kill();
+				//object1.dead = true;
+				//object1.visible = false;
+				((Enemy)object1).exists = false;
+				Level.switchLevel("lvl_test","");
 			}
-		}		
+
+			
+		}
+		
+		
 		);
 	}
 }
